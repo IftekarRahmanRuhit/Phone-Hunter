8ff527cce0dab78b21676a0898d4e3b1ab95660b
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-10-04 21:03:39 +0600

    added search functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// Declaring a function to load data from api 
-const loadPhone = async(isShowAll)=>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+
+// Declaring a function to load data from API
+const loadPhone = async(searchText = 'iphone',isShowAll)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data
     displayPhones(phones ,isShowAll);
@@ -10,64 +11,87 @@ const loadPhone = async(isShowAll)=>{
 
 // Displaying phones on the page 
 const displayPhones = (phones, isShowAll) => {
+
+    const phoneCards = document.getElementById('phoneCards');
+    phoneCards.innerHTML = '';
+   
     let showPhone = phones;
+
+    if(showPhone.length === 0){
+        // Showing no-item image when no items are found
+        document.getElementById('no-item').classList.remove('hidden');
+        document.getElementById('show-all-button-container').classList.add('hidden');
+        return;
+
+    }
+    else{
+        // Hiding no-item image when items are found
+        document.getElementById('no-item').classList.add('hidden');
+    }
+
     if(!isShowAll){
         showPhone= showPhone.slice(0, 6);
         document.getElementById('show-all-button-container').classList.remove('hidden')
     }
     else{
-        showPhone= showPhone.slice(6);
+        showPhone= showPhone.slice(7);
         document.getElementById('show-all-button-container').classList.add('hidden')
     }
+
     showPhone.forEach((item) =>{
         
         const phoneCard = document.createElement('div')
         phoneCard.innerHTML=`
-                    <div class="border p-4 flex flex-col justify-start items-center space-y-4 rounded-lg bg-slate-50 hover:border-slate-400 hover:cursor-pointer">
+        <div class="border p-4 flex flex-col justify-start items-center space-y-4 rounded-lg bg-slate-50 hover:border-slate-400 hover:cursor-pointer">
                 <div class="p-7 bg-[#0D6EFD0D] rounded-md">
-                    <img src="${item.image}" alt="">
+                    <img src="${item.image}" alt="${item.phone_name}">
                 </div>
-                <h3 class="text-xl font-semibold">${item.
-                    phone_name}</h3>
-                <p class="text-center font-semibold">There are many variations of passages of <br> available, but the majority have suffered</p>
+                <h3 class="text-xl font-semibold">${item.phone_name}</h3>
+                <p class="text-center font-semibold">There are many variations of passages of text available, but the majority have suffered.</p>
                 <p class="text-center font-bold text-2xl">$999</p>
                 <div>
-                    <button onclick="(loadDetails('${item.slug}'))" class="btn bg-[#0D6EFD] text-white ">Show Details</button>
-                  </div>
-                  
-
+                    <button onclick="loadDetails('${item.slug}')" class="btn bg-[#0D6EFD] text-white">Show Details</button>
+                </div>
+            </div>                 
         `
         phoneCards.append(phoneCard); 
     })
 }
-// calling the loadPhone Function 
+// Calling the loadPhone Function 
 loadPhone();
 
 
 // spinner 
 const handleSearch = ()=>{
-    document.getElementById('spinner').style.display='block'
+    document.getElementById('spinner').style.display='block';
+    const searchText = document.getElementById('search-input').value;
     setTimeout(function() {
-        loadPhone()
+        if (searchText) {
+            loadPhone(searchText); //Calling loadPhone function with search input
+        } else {
+            loadPhone(); // Calling loadPhone function for show Default iphone
+        }
 },1000)
 }
 
 // show more Button 
 
 const handleShowAll = ()=>{
-    loadPhone(true);
+    const searchText = document.getElementById('search-input').value || 'iphone';
+    loadPhone(searchText, true);
+
 }
 
-// show details 
+// Loading Details From API  
 const loadDetails = async(id) => {
     const uri = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(uri);
     const data = await res.json();
     const phone = data.data 
     displayDetails(phone);
-    
   };
   
+
   const displayDetails = (phone) => {
 
     const displayDetailsContainner = document.getElementById('modal-content');
@@ -92,5 +116,6 @@ const loadDetails = async(id) => {
     `
     my_modal_5.showModal();
 
-    // document.getElementById('customModal').showModal();
   };
+
+  
